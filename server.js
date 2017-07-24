0c1bec51a1fc9f36f0eeb0a801f1a8320dd5541b
Ian Katsuno <katsunoi@MBPe15C02QMR7FFVH3.local>
2017-07-24 16:52:14 -0400

    comitted backend and front end

---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('express')(),
+	  request = require('request'),
+	  cheerio = require('cheerio'),
+	 mongoose = require('mongoose'),
+	   agenda = require('agenda'),
+	   config = require('config'),
+	 dbConfig = config.get('Dev.dbConfig')
+     PORT = process.env.PORT || 8080
+
+mongoose.connect('mongodb://localhost/data/db/')
+mongoose.Promise = global.Promise
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', () => {
+		console.log("Connected to db at /data/db/")
+})
+
+app.get('/', (req,res)=>{
+	    console.log('Request on the index.')
+	    res.send('We wylin out.')
+})
+    
+app.listen(PORT, ()=>{
+	    console.log(`Express listening on ${PORT}`)
+	    console.log(`CTRL+C to kill process.`)
+})
