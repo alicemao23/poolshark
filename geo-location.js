c1f08ce7f242b8d633876ab0604a9841ed74e1a4
BrainStation <brainstation@BrainStations-MacBook-Air.local>
2017-08-15 16:36:57 -0400

    added script for getting lat/lng for each pool address in the pools.csv file

---
+++ geo-location.js
@@ -0,0 +1,42 @@
+const axios = require('axios'),
+    csv = require('csvtojson'),
+    json2csv = require('json2csv'),
+    fs = require('fs'),
+    API_KEY = '&key=AIzaSyBfxUPn3ixXu0v_zngcuxii0_iTt6HrvhA',
+    BASE_URL="https://maps.googleapis.com/maps/api/geocode/json?address=", 
+    csvFilePath='./pools.csv';
+
+let promises = [];
+csv()
+    .fromFile(csvFilePath)
+    .on('json',(jsonObj)=>{
+        promises.push(getGeoInfo(jsonObj))
+    
+    })
+    .on('done',(error)=> {
+        Promise.all(promises).then(function(values) {
+            writeToFile(values);
+        })
+        .catch((err)=> console.log(err) );
+    });
+
+function getGeoInfo(data){
+    return new Promise((resolve, reject)=> {
+        let address = data.Address+ ", Toronto, ON";
+        let result = address.split(' ').join('+');
+        axios.get(BASE_URL+result+API_KEY)
+        .then(response=> {
+            data["lat"] = response.data.results[0].geometry.location.lat; 
+            data["lng"] = response.data.results[0].geometry.location.lng;
+            resolve(data);
+        })
+        .catch(err => reject('err here, ', err))
+    })
+}
+
+function writeToFile(data){
+    let fields = ["Location","Address","Phone","Open Date","Close Date","lat","lng"]; 
+    let result = json2csv({data: data, fields: fields});
+    let writeStream = fs.createWriteStream('./finalpool.csv');
+    writeStream.write(result);
+}
